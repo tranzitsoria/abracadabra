f854a281f815e0c86f4299f8e525f41f61e87814
Taras <taras@Taras-Machine.(none)>
2012-08-16 19:40:42 +0300

    My First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abrakadabra::Application.config.secret_token = 'd35b9c234c815cd3415d7cefeaef0ceb8653f8ea726f46228fe4439e6fe2fa82eb854b49cb1efd8d02767d15b0f2cbdc068fda23c07d645c39f63985b68429db'
